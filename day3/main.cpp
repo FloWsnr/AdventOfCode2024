728c13657d7a382f71e773652c5ebdfa76355c4d
Florian Wiesner <wiesnerflorian94@gmail.com>
2024-12-07 16:00:50 +0100

    day 3 task 2

--- day3/main.cpp
+++ day3/main.cpp
@@ -5,7 +5,7 @@
 #include <regex>
 
 int main() {
-    std::ifstream file("input.txt"); // Open the file
+    std::ifstream file("input.txt");
     if (!file.is_open()) {
         std::cerr << "Failed to open the file." << std::endl;
         return 1;
@@ -13,24 +13,33 @@ int main() {
 
     std::stringstream buffer;
     buffer << file.rdbuf();
-    std::string content = buffer.str();
+    std::string text = buffer.str();
 
-    int num1, num2;
-    int product;
     int sum = 0;
-    // search for all the expressions mul(a,b)
-    std::regex mul_regex("mul\\(([0-9]+),([0-9]+)\\)");
+    bool enabled = true;  // mul instructions are enabled at the start
+
+    // Match mul(X,Y), do(), and don't() instructions
+    std::regex instruction_regex(R"((?:mul\((\d+),(\d+)\)|do\(\)|don't\(\)))");
+
     std::smatch match;
-    while (std::regex_search(content, match, mul_regex)) {
+    std::string::const_iterator searchStart(text.cbegin());
 
-        num1 = std::stoi(match.str(1));
-        num2 = std::stoi(match.str(2));
+    while (std::regex_search(searchStart, text.cend(), match, instruction_regex)) {
+        if (match[0] == "do()") {
+            enabled = true;
+        }
+        else if (match[0] == "don't()") {
+            enabled = false;
+        }
+        else if (enabled && match[1].matched) { // This is a mul() instruction and multiplication is enabled
+            int num1 = std::stoi(match[1]);
+            int num2 = std::stoi(match[2]);
+            sum += num1 * num2;
+        }
 
-        product = num1 * num2;
-        sum += product;
-
-        // remove the match from the content
-        content = match.suffix().str();
+        searchStart = match.suffix().first;
     }
-    std::cout << "Sum of all products: " << sum << std::endl;
+
+    std::cout << "Sum of all enabled products: " << sum << std::endl;
+    return 0;
 }
